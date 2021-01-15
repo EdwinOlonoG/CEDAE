811f5a68728b230fdb7aedfcbeb93e0f410b02a5
Brandon Gomez <brandonmerida11@gmail.com>
2021-01-14 23:24:48 -0600

    ahora si la buena

--- src/app/ventas/ventas.service.ts
+++ src/app/ventas/ventas.service.ts
@@ -27,9 +27,7 @@ export class VentasService {
     addVenta(venta: IVentas[]){   //aplico la venta
       console.log("Entre a la funcion de hacer la venta");
       console.log(venta);
-    this.http.get(`${this.baseUrl}postVentas.php?idSucursal=1`).subscribe(venta => {
-      console.log(venta);
-    });
+      this.http.post(`${this.baseUrl}postVentas.php?idSucursal=1`, venta).subscribe();
     this.http.get(`${this.baseUrl}updateProductos.php?idSucursal=1`).subscribe(venta => {
       console.log(venta);
     });
